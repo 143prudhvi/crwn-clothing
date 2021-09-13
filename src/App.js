9adddf9708d7bd9681c08d070b668f17906299f7
143prudhvi <prudhvinath1998@gmail.com>
2021-09-13 21:03:31 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import HomePage from './functional-components/homepage.component';
+import './functional-components/homepage.styles.scss';
+
+function App() {
+  return (
+    <div >
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
