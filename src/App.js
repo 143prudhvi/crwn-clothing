c71f2e763aea78fcb7eb54d339625952de234ca7
143prudhvi <prudhvinath1998@gmail.com>
2021-09-20 20:19:11 +0530

    Sign In and Sign Up

--- src/App.js
+++ src/App.js
@@ -3,22 +3,55 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
 import {Switch ,Route} from 'react-router-dom';
+import ShopPage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
-const Hats = () => (
-  <div>
-    <h1>Hats Available</h1>
-  </div>
-);
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      currentUser : null
+    }
+  }
 
-function App() {
-  return (
-    <div >
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={Hats} />
-      </Switch>
-    </div>
-  );
+  unSubscribeFromAuth = null
+
+  componentDidMount(){
+    this.unSubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({currentUser : {
+              id : snapshot.id,
+              ...snapshot.data()
+            }
+          });
+        });
+      }
+      this.setState({currentUser : userAuth});
+    });
+  }
+
+  componentWillUnmount(){
+    this.unSubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div >
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
